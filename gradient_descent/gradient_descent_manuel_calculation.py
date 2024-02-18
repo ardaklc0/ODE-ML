fix gradient

gradient_descent/gradient_descent_manuel_calculation.py
@@ -54,19 +54,29 @@ def gradient_descent(X, y, theta, learning_rate=0.01, iterations=100):
     
     Returns the final theta vector and array of cost history over no of iterations
     '''
-    m = len(y)
-    cost_history = np.zeros(iterations)
-    theta_history = np.zeros((iterations,2))
-    for it in range(iterations):
-        prediction = np.dot(X, theta)
+    m = len(y) # no of samples
+    cost_history = np.zeros(iterations) # np.zeros creates an array of zeros with the given shape
+    theta_history = np.zeros((iterations,2)) # np.zeros creates an array of zeros with the given shape
+    for i in range(iterations):
+        prediction = np.dot(X, theta) 
         theta = theta -(1/m)*learning_rate*( X.T.dot((prediction - y)))
-        theta_history[it,:] = theta.T
-        cost_history[it]  = calculate_cost(theta,X,y)
-        
+        theta_history[i,:] = theta.T
+        cost_history[i]  = calculate_cost(theta,X,y)
+
+    print(theta)
+    print(cost_history)
+    print(theta_history)
+
     return theta, cost_history, theta_history
 
 X = 2 * np.random.rand(100,1)
-y = 4 +3 * X+np.random.randn(100,1)
+y = 4 + 3 * X + np.random.randn(100,1) # y = 8 + 24x + noise to make it more realistic and challenging
+# y = 4 + 3 * X # y = 4 + 3x (x is element of X = 2 * np.random.rand(100,1))
+
+# plt.plot(X,y,'b.')
+# plt.ylabel('Y')
+# plt.xlabel('X')
+# plt.show()
 
 lr = 0.01
 n_iter = 1000
@@ -76,27 +86,25 @@ theta = np.random.randn(2,1)
 print(theta)
 
 X_b = np.c_[np.ones((len(X),1)),X]
-theta,cost_history,theta_history = gradient_descent(X_b,y,theta,lr,n_iter)
+theta,cost_history,theta_history = gradient_descent(X_b, y, theta,lr,n_iter)
 print(X_b)
 
 print('Theta0:          {:0.3f},\nTheta1:          {:0.3f}'.format(theta[0][0],theta[1][0]))
 print('Final cost/MSE:  {:0.3f}'.format(cost_history[-1]))
 
-fig = plt.figure(figsize=(10,8),dpi=100)
-fig.subplots_adjust(hspace=0.4, wspace=0.4)
-
-it_lr =[(2000,0.001),(500,0.01),(200,0.05),(100,0.1)]
-count =0
-
-for n_iter, lr in it_lr:
-    count += 1
+# fig = plt.figure(figsize=(10,8),dpi=100)
+# fig.subplots_adjust(hspace=0.4, wspace=0.4)
+# it_lr =[(2000,0.001),(500,0.01),(200,0.05),(100,0.1)]
+# count =0
+# for n_iter, lr in it_lr:
+#     count += 1
     
-    ax = fig.add_subplot(4, 2, count)
-    count += 1
+#     ax = fig.add_subplot(4, 2, count)
+#     count += 1
    
-    ax1 = fig.add_subplot(4,2,count)
+#     ax1 = fig.add_subplot(4,2,count)
     
-    ax.set_title("lr:{}".format(lr))
-    ax1.set_title("Iterations:{}".format(n_iter))
-    plot_GD(n_iter,lr,ax,ax1)
-plt.show()
+#     ax.set_title("lr:{}".format(lr))
+#     ax1.set_title("Iterations:{}".format(n_iter))
+#     plot_GD(n_iter,lr,ax,ax1)
+# plt.show()